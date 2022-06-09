d3794550320997e7b3819a0688ea07fac4a5dd7e
John Macka <macka2004@gmail.com>
2022-06-10 08:09:31 +1000

    lmi: adapt/bring up p404 for lmi

--- p404_lmi.mk
+++ p404_lmi.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the device configuration.
-$(call inherit-product, device/xiaomi/cmi/device.mk)
+$(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit from the 404 configuration.
 $(call inherit-product, vendor/404/configs/common.mk)
@@ -30,12 +30,12 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Include GAPPS
 WITH_GAPPS := true
 
-PRODUCT_NAME := p404_cmi
-PRODUCT_DEVICE := cmi
+PRODUCT_NAME := p404_lmi
+PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 10 Pro
+PRODUCT_MODEL := POCO F2 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cmi/cmi:12/SKQ1.211006.001/V13.0.3.0.SJACNXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/lmi/lmi:12/RKQ1.211001.001/V13.0.1.0.SJKMIXM:user/release-keys
